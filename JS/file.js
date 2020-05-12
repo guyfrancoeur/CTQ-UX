5afb06b6ffc6ee7e17640eba0d0db75b427bd662
Marie287 <marie.bruggeman287@gmail.com>
2020-05-12 17:37:32 +0200

    Langues

--- JS/file.js
+++ JS/file.js
@@ -1,4 +1,4 @@
-/* Espace résultats */
+/* ********* Espace résultats ********* */
 function modify(){
 	$('#container').addClass('col-12');
 	$('#div1').addClass('row');
@@ -16,7 +16,7 @@ function modify(){
 
 
 
-/* LOGIN (modal) */
+/* ********* LOGIN (modal) ********* */
 $(function(){
   $('#login-form').submit(function(e) {
     e.preventDefault()
@@ -35,3 +35,87 @@ $(function(){
   })
 })
 
+
+
+
+/* ********* Languages ********* */
+
+var languages = new Array();
+
+languages['fr'] = {
+	"home" : "Accueil",
+	"sign-in" : "Se connecter",
+	"password" : "Mot de passe",
+	"sing-up" : "Nouveau ici ? S'inscrire",
+	"forgot" : "Mot de passe oublié",
+	"quantity" : "Quantité",
+	"weight" : "Poids",
+	"sign-in2" : "Se connecter",
+	"time" : "Temps",
+	"value" : "Valeur",
+	"hours" : "heures",
+	"dollar" : "€",
+	"longueur" : "Longueur",
+	"width" : "Largeur",
+	"height" : "Hauteur",
+	"requirements" : "Exigences",
+	"loading" : "Chargement",
+	"dimensions" : "Dimensions du fret (pieds)",
+	"search" : "Rechercher",
+	"origin" : "DÉPART - Ville ou Code Postal",
+	"destination" : "DESTINATION - Ville ou Code Postal",
+	"login-title" : "Se connecter",
+	"refrigerated" : "réfrigéré"
+}
+
+languages['en'] = {
+	"home" : "Home",
+	"sign-in" : "Sign in",
+	"password" : "Password",
+	"sing-up" : "New around here ? Sign up",
+	"forgot" : "Forgot password ?",
+	"quantity" : "Quantity",
+	"weight" : "Weight",
+	"sign-in2" : "Sign in",
+	"time" : "Time",
+	"value" : "Value",
+	"hours" : "hours",
+	"dollar" : "$",
+	"longueur" : "Length",
+	"width" : "Width",
+	"height" : "Height",
+	"requirements" : "Requirements",
+	"loading" : "Loading",
+	"dimensions" : "Freight dimensions (feet)",
+	"search" : "Search",
+	"origin" : "ORIGIN - City or Postal Code",
+	"destination" : "DESTINATION - City or Postal Code",
+	"login-title" : "Login",
+	"refrigerated" : "refrigerated"
+}
+
+
+$(function() {
+    $('.translate').click(function() {
+      var lang = $(this).attr('id');
+
+      $('.lang').each(function(index, item) {
+        $(this).text(languages[lang][$(this).attr('id')]);
+        $(this).attr('placeholder', languages[lang][$(this).attr('id')]); 
+      });
+    });
+    
+    $(".other-lang-button").click(function(){
+    	var val1 = $(".lang-button").text();
+    	var id1 = $(".lang-button").attr("id");
+    	var val2 = $(".other-lang-button").text();
+    	var id2 = $(".other-lang-button").attr("id");
+    	
+    	$(".other-lang-button").text(val1);
+    	$(".other-lang-button").val(val1);
+    	$(".other-lang-button").attr("id",id1);
+        $(".lang-button").text(val2);
+        $(".lang-button").val(val2);
+        $(".lang-button").attr("id",id2);
+    });
+  }); 
