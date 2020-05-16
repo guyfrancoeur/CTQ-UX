f745f4ef3c53c613fdf7566515ace63c9efc42a2
Marie287 <marie.bruggeman287@gmail.com>
2020-05-16 21:48:04 +0200

    Validation formulaire

--- js/file.js
+++ js/file.js
@@ -1,3 +1,25 @@
+/* ********* FORM VALIDATION ********* */
+(function() {
+	'use strict';
+	window.addEventListener('load', function() {
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName('needs-validation');
+		// Loop over them and prevent submission
+		var validation = Array.prototype.filter.call(forms, function(form) {
+			form.addEventListener('submit', function(event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add('was-validated');
+			}, false);
+		});
+	}, false);
+})();
+
+
+
+/* ********* Sécurité Login ********* */
 $("#nsecure").slider({tooltip: 'always'}); //a la JQuery
 
 $("#nsecure").change(function(){
@@ -22,28 +44,6 @@ function modify(){
 		
 	document.getElementById("contenu").innerHTML += "<h3>Results</h3>";
 }
-
-
-
-/* ********* FORM VALIDATION ********* */
-(function() {
-	'use strict';
-	window.addEventListener('load', function() {
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = document.getElementsByClassName('needs-validation');
-		// Loop over them and prevent submission
-		var validation = Array.prototype.filter.call(forms, function(form) {
-			form.addEventListener('submit', function(event) {
-				if (form.checkValidity() === false) {
-					event.preventDefault();
-					event.stopPropagation();
-				}
-				form.classList.add('was-validated');
-			}, false);
-		});
-	}, false);
-})();
-
 
 
 
