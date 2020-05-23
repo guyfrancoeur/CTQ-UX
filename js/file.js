bf1fd76ba81625d6884e0d62119e47cec904d4d5
Marie287 <marie.bruggeman287@gmail.com>
2020-05-23 19:12:19 +0200

    correction

--- js/file.js
+++ js/file.js
@@ -1,25 +1,23 @@
 /* ********* Languages ********* */
-
 $(function() {
-	$('.translate').click(function() {
+  $('.translate').click(function() {
     var lang = $(this).attr('id');
     $.ajax({
-	    url: "./data/" + lang + ".json",
-	    dataType: "json",
-	    success:function(data){
-	    	
-				$.each(data, function(index, x) {
-					$(x.obj).text(x.value);
-					$(x.obj).attr('placeholder', x.value);
-					$(x.obj).attr('title', x.value);
-				});
-	    },
-	    error:function(xhr, ajaxOptions, thrownError){
-	  	  console.log('error');
-	      console.log(thrownError);
-	    }
-	  })
-	})
+      url: "./data/" + lang + ".json",
+      dataType: "json",
+      success:function(data){	
+        $.each(data, function(index, x) {
+          $(x.obj).text(x.value);
+          $(x.obj).attr('placeholder', x.value);
+          $(x.obj).attr('title', x.value);
+        });
+      },
+      error:function(xhr, ajaxOptions, thrownError){
+        console.log('error');
+        console.log(thrownError);
+      }
+    })
+  })
 });
 
 
