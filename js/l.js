04df26b783805ac6aa96cd1ff295d7f988f46054
Marie287 <marie.bruggeman287@gmail.com>
2020-05-25 17:00:51 +0200

    test login

--- js/l.js
+++ js/l.js
@@ -6,3 +6,28 @@ $("#nsecure").change(function(){
     $("#bsubmit").prop('disabled', false);
   }
 });
+
+// Validation login
+$("#bsubmit").click(function() {
+	var inputEmail = $("#cemail").val();
+	var inputPassword = $("cpassword").val(); 
+	$.ajax({
+      url: "./data/login.json",
+      dataType: "json",
+      success:function(data){	
+        $.each(data.login, function(index, x) {
+        	if((inputEmail == x.cemail) && (inputPassword == x.cpassword)){
+        		url = "http://salutem.co/CTQ-UX/d.html";
+            $("#bsubmit").attr("href", url);
+            return true;
+          }
+        });
+        alert("email ou mot de passe invalide")
+        return false;
+      },
+      error:function(xhr, ajaxOptions, thrownError){
+        console.log('error');
+        console.log(thrownError);
+      }
+    })
+});
