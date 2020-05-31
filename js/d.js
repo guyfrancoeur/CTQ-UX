73d72cdd04c88ed23d63d92d51f87aa34918ec69
Marie287 <marie.bruggeman287@gmail.com>
2020-05-31 16:32:01 +0200

    test modale

--- js/d.js
+++ js/d.js
@@ -377,14 +377,8 @@ $("#clongueur").focusout(function() {
 });
 
 // Lancement modale
-$('#addcourriel').click(function(event) {
-  $('#m_courriels').load('./m_courriels.html', function(response, status, xhr) {
-  	if ( status == "error" ) {
-	    var msg = "Sorry but there was an error: ";
-	    $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
-	  }
-  	$('#m_courriels').modal('show');
-  });
+$('#m_courriels').load('./m_courriels.html', function() {
+	$('#m_courriels').modal('show');
 });
 
 
