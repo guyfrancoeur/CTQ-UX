7cff3dd7e9ff0284e1a96cfd94cfe27f07a2306f
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2020-06-02 17:44:18 -0400

    version initiale

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//exemple long
+$( document ).ready(function() {
+  //$('#m_profil').load('../modal/m.profil.html', function () { m_profil_ready('#m_profil'); });
+  $('#m_template').load('./m.template.html');
+  $('#m_broker').load('./m.broker.html');
+  $('#m_trucker').load('./m.trucker.html');
+});
+
+//exemple court
+$(function() {
+  console.log( "ready!" );
+});
