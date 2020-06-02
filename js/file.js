43f3f3fa1a4ea99f3fddefeba267bac6a9e7045b
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2020-06-02 17:15:32 -0400

    test

--- js/file.js
+++ js/file.js
@@ -1,5 +1,5 @@
 /* ********* Languages ********* */
-$(function() {
+//$(function() {
   $('.translate').click(function() {
     var lang = $(this).attr('id');
     $.ajax({
@@ -19,11 +19,14 @@ $(function() {
       }
     })
   })
-});
+//});
 
+//Je ne comprend pas pourquoi deux facon de faire.
+//Que fait cette fonction ?
+//Utile pour quel formulaire ?
 // Validation formulaire
-(function() {
-  'use strict';
+//(function() {
+//  'use strict';
   window.addEventListener('load', function() {
     var forms = document.getElementsByClassName('needs-validation');
     var validation = Array.prototype.filter.call(forms, function(form) {
@@ -36,7 +39,7 @@ $(function() {
       }, false);
     });
   }, false);
-})();
+//})();
 
 // Animation index.html
 $(".arrow-card" ).click(function() {
