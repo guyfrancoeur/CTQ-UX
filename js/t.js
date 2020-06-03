cc442f73eef8765339923d58a994cb4a4aec9a6c
Marie287 <marie.bruggeman287@gmail.com>
2020-06-03 23:52:49 +0200

    ajouts

---
+++ js/t.js
@@ -0,0 +1,14 @@
+// Edit location
+function editable(x) {
+  var text1 = x.prev();
+  $(text1).prop('contenteditable',true);
+}
+
+$('.bset-location').click(function() {
+	editable($(this));
+  $(".cpostalcode").focus();
+});
+
+$(".cpostalcode").focusout(function() {
+	$(this).prop('contenteditable',false);
+});
