d922cf076800a3284ab9617dae81002f3a007a44
Marie287 <marie.bruggeman287@gmail.com>
2020-06-04 00:48:59 +0200

    ajouts

--- js/t.js
+++ js/t.js
@@ -1,14 +1,21 @@
+$( document ).ready(function() {
+  $('#m_ajoutcamion').load('./m.ajoutcamion.html');
+});
+
 // Edit location
 function editable(x) {
   var text1 = x.prev();
   $(text1).prop('contenteditable',true);
 }
-
 $('.bset-location').click(function() {
-	editable($(this));
+  editable($(this));
   $(".cpostalcode").focus();
 });
-
 $(".cpostalcode").focusout(function() {
-	$(this).prop('contenteditable',false);
+  $(this).prop('contenteditable',false);
+});
+
+// Bouton trash (suppression ligne)
+$('.btrash').click(function() {
+  $(this).closest( "tr" ).remove();
 });
