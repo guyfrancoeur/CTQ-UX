37ad3a1825b9d286b63bdf3b2aa646016ff7ed2e
Marie287 <marie.bruggeman287@gmail.com>
2020-06-04 19:05:15 +0200

    ajouts

--- js/t.js
+++ js/t.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
   $('#m_ajoutcamion').load('./m.ajoutcamion.html');
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
 // Edit location
@@ -18,4 +19,19 @@ $(".cpostalcode").focusout(function() {
 // Bouton trash (suppression ligne)
 $('.btrash').click(function() {
   $(this).closest( "tr" ).remove();
+});
+
+// Récupérer les valeurs de la ligne quand on clique sur le crayon
+$('.bset').click(function() {
+	var current_col = $(this).closest( "tr" );
+	var tracteur = $(current_col).find("td").eq(0).html();
+	var equipement = $(current_col).find("td").eq(1).html();
+	$('#selectTracteur').val(tracteur).change();
+	$('#selectEquipement').val(equipement).change();
+});
+
+// Bouton ajout camion, réinitialiser la sélection (dropdown)
+$('#bajoutcamion').click(function() {
+	$("#selectTracteur").val("").change();
+	$("#selectEquipement").val("").change();
 });
