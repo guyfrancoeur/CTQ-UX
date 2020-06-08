4d04980a60e4986dc12a171dbd3c7aafff863047
Marie287 <marie.bruggeman287@gmail.com>
2020-06-08 22:42:31 +0200

    correction bogue traduction

--- js/g.function.js
+++ js/g.function.js
@@ -1,22 +1,20 @@
 /* ********* Languages ********* */
-function traduire() {
-  $('.translate').click(function() {
-    var lang = $(this).attr('id');
-    $.ajax({
-      url: "./data/" + lang + ".json",
-      dataType: "json",
-      success:function(data){	
-        $.each(data, function(index, x) {
-          if(x.propriete == "text") $(x.obj).text(x.value);
-          else{
-            $(x.obj).attr(x.propriete, x.value);
-          }
-        });
-      },
-      error:function(xhr, ajaxOptions, thrownError){
-        console.log('error');
-        console.log(thrownError);
-      }
-    });
+$('.translate').click(function() {
+  var lang = $(this).attr('id');
+  $.ajax({
+    url: "./data/" + lang + ".json",
+    dataType: "json",
+    success:function(data){ 
+      $.each(data, function(index, x) {
+        if(x.propriete == "text") $(x.obj).text(x.value);
+        else{
+          $(x.obj).attr(x.propriete, x.value);
+        }
+      });
+    },
+    error:function(xhr, ajaxOptions, thrownError){
+      console.log('error');
+      console.log(thrownError);
+    }
   });
-}
+});
