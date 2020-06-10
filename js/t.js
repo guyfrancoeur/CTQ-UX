b58fef15952e63b9d0b51b9fc194d68f214a453c
Marie287 <marie.bruggeman287@gmail.com>
2020-06-11 00:06:28 +0200

    input tableau camions + edit/ajout équipement dans formulaire

--- js/t.js
+++ js/t.js
@@ -14,21 +14,6 @@ $( document ).ready(function() {
   $(".bX").hide();
 });
 
-// Edit location
-function editable(x) {
-  var text1 = x.prev();
-  $(text1).prop('contenteditable',true);
-  $(text1).addClass("setprofile");
-}
-$('.bset-location').click(function() {
-  editable($(this));
-  $(".cpostalcode").focus();
-});
-$(".cpostalcode").focusout(function() {
-  $(this).prop('contenteditable',false);
-  $(this).removeClass("setprofile");
-});
-
 // Bouton trash (suppression ligne)
 $('.btrash').click(function() {
   $(this).closest( "tr" ).remove();
@@ -72,7 +57,7 @@ function camionEvent(){
       '</td>' +
       '<td>'+ choix_tracteur +'</td>'+
       '<td>'+ choix_equipement +'</td>' +
-      '<td><span class="cpostalcode" contenteditable="false">XXX</span> <button type="button" class="bset-location btn p-0"><i class="fas fa-map-marker-alt color-icon"></i></button></td>'+
+      '<td><input type="text" class="form-control form-control-sm m-auto cpostalcode" value="H3N"></td>'+
       '<th scope="row" class="text-right">'+
         '<button type="button" class="btn p-0 bset" data-toggle="modal" data-target="#m_c"><i class="fas fa-pencil-alt color-icon"></i></button>'+
         '<button type="button" class="btn p-0 btrash"><i class="fas fa-trash color-icon"></i></button>' +
@@ -112,32 +97,11 @@ function validerprofil(){
 }
 
 // Fonctions qui concernent la modale ajout équipement
-$('#m_e').on('shown.bs.modal', function() {
-  //Cacher boutons validations (modale ajout équipement)
-  $(".bV").hide();
-  $(".bX").hide();
-  
-  // Couleur tableau ajout équipement (vert)
+$('#m_e').on('shown.bs.modal', function() {  
+  // Couleur tableau liste équipement (header)
   $('.hclass').click(function() {
     $(this).addClass("headcolor");
     $(".hclass").not(this).removeClass("headcolor");
-  });
-  
-  // Boutons modification équipement (crayon)
-  $('.bS').click(function() {
-    $(this).closest( "div" ).next("div").find(".valuequipement").prop('contenteditable',true);
-    $(this).closest("div").next("div").find(".valuequipement").addClass("setprofile");
-    $(this).closest( "div" ).next("div").find(".valuequipement").eq(0).focus();
-    $(".bV").show();
-    $(".bX").show();
-  });
-  
-  // Bouton validation modification équipement (V)
-  $('.bV').click(function() {
-    $(this).closest( "div" ).prev("div").find(".valuequipement").prop('contenteditable',false);
-    $(this).closest("div").prev("div").find(".valuequipement").removeClass("setprofile");
-    $(".bV").hide();
-    $(".bX").hide();
   });
   
   // Bouton suppression équipement (trash)
@@ -145,42 +109,82 @@ $('#m_e').on('shown.bs.modal', function() {
     $(this).closest(".card").remove();
   });
   
+  var mode_save_button_equipement = "add";
+  var nE, cE, loE, laE, hE;
+  // Boutons modification équipement (crayon)
+  $('.bS').click(function() {
+    mode_save_button_equipement = "set";
+    // Récupérer les valeurs de l'équipement en question
+    nE = $(this).closest( "span" ).closest( "h5" ).closest( "div" ).next("div").find("div").find("p").eq(0).find(".valuequipement");
+    cE = $(this).closest( "span" ).closest( "h5" ).closest( "div" ).next("div").find("div").find("p").eq(1).find(".valuequipement");
+    loE = $(this).closest( "span" ).closest( "h5" ).closest( "div" ).next("div").find("div").find("p").eq(2).find(".valuequipement");
+    laE = $(this).closest( "span" ).closest( "h5" ).closest( "div" ).next("div").find("div").find("p").eq(3).find(".valuequipement");
+    hE = $(this).closest( "span" ).closest( "h5" ).closest( "div" ).next("div").find("div").find("p").eq(4).find(".valuequipement");
+    // Mettre ces valeurs dans le formulaire
+    $('#cnomE').val(nE.html()).change();
+    $('#ccapaciteE').val(cE.html()).change();
+    $('#clongueurE').val(loE.html()).change();
+    $('#clargeurE').val(laE.html()).change();
+    $('#chauteurE').val(hE.html()).change();
+  });
+  
+  function editEqui(){
+    // Sauvegarder la modification (changement des valeurs dans la liste des équipements)
+    nE.html($('#cnomE').val());
+    cE.html($('#ccapaciteE').val());
+    loE.html($('#clongueurE').val());
+    laE.html($('#clargeurE').val());
+    hE.html($('#chauteurE').val());
+    nE.closest("p").closest("div").closest(".collapse").prev().find("h5").find("span").eq(0).html($('#cnomE').val());
+    $('#cnomE').val(""); // Remettre le formulaire à zéro
+    $('#ccapaciteE').val("");
+    $('#clongueurE').val("");
+    $('#clargeurE').val("");
+    $('#clargeurE').val("");
+    $('#chauteurE').val("");
+    mode_save_button_equipement = "add";
+  }
+  
   // Ajouter un équipement
-  $("#baddEquipement").click(function() {
-    var nomE = $("#cnomE").val();
-    var capactiteE = $("#ccapaciteE").val();
-    var longueurE = $("#clongueurE").val();
-    var largeurE = $("#clargeurE").val();
-    var hauteurE = $("#chauteurE").val();
-    
-    var html = '<div class="card">' +
-      '<div class="card-header hclass" id="heading' + nomE + '" data-toggle="collapse" data-target="#collapse' + nomE + '" aria-expanded="true" aria-controls="collapse' + nomE + '"> '+
-        '<h5 class="mb-0">' + nomE + '</h5> ' + 
-      '</div>' +
-      '<div id="collapse' + nomE + '" class="collapse" aria-labelledby="heading' + nomE + '" data-parent="#accordion">' +
-        '<div class="card-body py-1">' +
-          '<div class="row">' +
-            '<div class="justify-content-center d-flex flex-column align-items-center mr-2 flex-shrink-1">' +
-              '<button type="button" class="btn px-1 bS"><i class="fas fa-pen color-icon"></i></button>' +
-              '<button type="button" class="btn px-1 bP"><i class="fas fa-trash color-icon"></i></button>' +
-            '</div>' +
-            '<div class="pl-2">' +
-              '<p><span class="labelcontent">Name</span><span class="spanclass">: </span><span class="valuequipement">' + nomE + '</span></p>' +
-              '<p><span class="labelcontent">Capacity</span><span class="spanclass">: </span><span class="valuequipement">' + capactiteE + ' kg</span></p>' +
-              '<p><span class="labelcontent">Length</span><span class="spanclass">: </span><span class="valuequipement">' + longueurE + ' m</span></p>' +
-              '<p><span class="labelcontent">Width</span><span class="spanclass">: </span><span class="valuequipement">' + largeurE + ' m</span></p>' +
-              '<p><span class="labelcontent">Height</span><span class="spanclass">: </span><span class="valuequipement">' + hauteurE + ' m</span></p>' +
-            '</div>' +
-            '<div class="justify-content-center d-flex flex-column align-items-center ml-auto pr-2">' +
-              '<button type="button" class="btn px-1 bV"><i class="fas fa-check fa-lg" style="color:#7FB23B;"></i></button>' +
-              '<button type="button" class="btn px-1 bX"><i class="fas fa-times-circle fa-lg" style="color:#AF2020;"></i></button>' +
-            '</div>'
-          '</div'> +
-        '</div'> +
-      '</div'> +
+  function addEqui(){
+    console.log("2");
+      var nomE = $("#cnomE").val();
+      var capactiteE = $("#ccapaciteE").val();
+      var longueurE = $("#clongueurE").val();
+      var largeurE = $("#clargeurE").val();
+      var hauteurE = $("#chauteurE").val();
+      var html = '<div class="card">' +
+        '<div class="card-header hclass" id="heading' + nomE + '" data-toggle="collapse" data-target="#collapse' + nomE + '" aria-expanded="true" aria-controls="collapse' + nomE + '">' +
+           '<h5 class="mb-0">' +
+              '<span>' + nomE + '</span>' +
+              '<span class="float-right" style="margin-top:-5px;">' +
+                '<button type="button" class="btn p-0 bS"><i class="fas fa-pen color-icon"></i></button>' +
+                '<button type="button" class="btn py-0 pl-2 bP"><i class="fas fa-trash color-icon"></i></button>' +
+              '</span>' +
+          '</h5>' +
+        '</div>' +
+        '<div id="collapse' + nomE + '" class="collapse" aria-labelledby="heading' + nomE + '" data-parent="#accordion">' +
+           '<div class="card-body py-1">' +
+              '<p><span class="labelcontent lcname">Name</span> : <span class="valuequipement">' + nomE + '</span></p>' +
+              '<p><span class="labelcontent lccapacity">Capacity</span> : <span class="valuequipement">' + capactiteE + ' kg</span></p>' +
+              '<p><span class="labelcontent lclength">Length</span> : <span class="valuequipement">' + longueurE + ' m</span></p>' +
+              '<p><span class="labelcontent lcwidth">Width</span> : <span class="valuequipement">' + largeurE + ' m</span></p>' +
+              '<p><span class="labelcontent lcheight">Height</span> : <span class="valuequipement">' + hauteurE + ' m</span></p>' +
+           '</div>' +
+        '</div>' +
       '</div>';
-    $('#accordion').append(html);
-    $('#selectEquipement').append('<option value="' + nomE + '">' + nomE + '</option>');
-    
+      $('#accordion').append(html);
+      $('#selectEquipement').append('<option value="' + nomE + '">' + nomE + '</option>');
+  }
+  
+  $("#baddEquipement").click(function() {
+    if (mode_save_button_equipement == "add"){
+      addEqui();
+      console.log("1");
+    }
+    else{
+      editEqui();
+    }
   });
 });
+
